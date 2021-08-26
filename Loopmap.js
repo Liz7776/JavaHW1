ceb92de6685b8884fe9d2df4a5472246e4f86695
Liz7776 <86306850+Liz7776@users.noreply.github.com>
2021-08-26 03:58:13 -0700

    Add files via upload

---
+++ Loopmap.js
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class LoopMapExercise {
+    public static void main(String[] args) {
+    Scanner scan = new this.Scanner(System.in);
+    int[] numArray;
+    
+    System.out.println("myNumber");
+     int numbers[]=new int[]{4,22,45,68,9};
+     int smallest=numbers[0];
+     int largest=numbers[0];
+     int product=numbers[0];
+
+     int sum= 4+22+45+68+9;
+    int numbArray[] = {num1, num2, num3, num4, num5};
+    int sum = 0;
+
+for (int i : numbArray)
+     sum += i;
+System.out.printIn("The sum of the numbers is " + sum);
+
+System.out.printIn("The product of the numbers is ");
+int numbers [] = new int[]{num1, num2, num3, num4, num5};
+int smallest = numbers [0]
+int largest = numbers [0]
+
+for (int i = 1; i < numbers.length; i++) {
+    if (numbers[i] > largest)
+        largest = numbers[i];
+        else if (numbers[i] < smallest)
+        smallest = numbers[i];
+
+    System.out.printIn("The largest Number is : "+ largest);
+    System.out.printIn("The smallest Number is : "+ smallest);
+}
+
+}
+
+}
